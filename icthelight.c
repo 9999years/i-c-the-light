1223c238ee0c4a1565d2352a33a8c8953d1b9447
Rebecca Turner <637275@gmail.com>
2016-08-08 15:18:34 -0400

    Moved sdlskeleton.c into icthelight.c, big readme changes.

--- icthelight.c
+++ icthelight.c
@@ -1,9 +1,19 @@
+//display
+#include "SDL/SDL.h"
+//logging, file out
 #include <stdio.h>
+//sin functions
 #include <math.h>
+//timestamp for file names
 #include <time.h>
-#include <stdbool.h>
+//int / array color conversion
+#include "color.h"
 
-#define ARRAYSIZE(arr) (sizeof(arr) / sizeof(arr[0]))
+//Screen dimension constants
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+#define HALF_SCREEN_WIDTH 320
+#define HALF_SCREEN_HEIGHT 240
 
 enum {
 	PORTABLE_BITMAP = 1,
@@ -11,122 +21,167 @@ enum {
 	PORTABLE_PIXMAP = 3
 };
 
-enum { RED, GREEN, BLUE };
-
-unsigned long int epoch()
-{
-	return time(NULL);
-}
-
-int clamp(
-	int value,
-	int min,
-	int max
-	)
-{
-	if(value < min)
-	{
-		return min;
-	}
-	else if(value > max)
-	{
-		return max;
-	}
-	else
-	{
-		return value;
-	}
-}
-
-int autoscale(
-	int width,
-	int height,
-	int channels,
-	unsigned char image[width][height][channels],
-	int min,
-	int max
-	)
-{
-	int i = 0, j = 0, k = 0;
-	for(i = 0; i < width; i++)
-	{
-		for(j = 0; j < height; j++)
-		{
-			for(k = 0; k < channels; k++)
-			{
-				image[i][j][k] -= min;
-				image[i][j][k] *= 0xff/(max-min);
-				image[i][j][k] = clamp(image[i][j][k], 0, 0xff);
-			}
-		}
-	}
-	return 0;
-}
-
 int writeppm(
 	char *filename,
 	char filetype,
 	int width,
 	int height,
-	int channels,
-	unsigned char image[width][height][channels]
+	unsigned int image[]
 	)
 {
 	FILE *file = fopen(filename, "w");
 	if(file == NULL)
 	{
 		printf("file open failure!\n");
+		return 1;
 	}
-	fprintf(file, "P%d\n%d %d\n%d\n", filetype, width, height, 256);
+	fprintf(file, "P%d\n%d %d\n%d\n", filetype, width, height, 0xff);
 	int i = 0, j = 0, k = 0;
-	for(i = 0; i < width; i++)
+	rgbcolor pixel;
+	for(i = 0; i < height; i++)
 	{
 		for(j = 0; j < width; j++)
 		{
-			for(k = 0; k < channels; k++)
+			inttocolor(pixel, image[j + i*width]);
+			for(k = 0; k < CHANNELS; k++)
 			{
-				fprintf(file, "%d ", image[i][j][k]);
+				fprintf(file, "%d ", pixel[k]);
 			}
 		}
 		fprintf(file, "\n");
 	}
+	printf("write done!\n");
 	return 0;
 }
 
-int main( int argc, char *argv[] )
+int render(SDL_Surface *screenSurface)
 {
-	//printf("time is %lu seconds", (unsigned long)time(NULL));
-	unsigned char image[256][256][3];
-	int i = 0, j = 0, k = 0, min = 255, max = 0;
-	for(i = 0; i < ARRAYSIZE(image); i++)
+	// Lock surface if
+	if(SDL_MUSTLOCK(screenSurface))
 	{
-		for(j = 0; j < ARRAYSIZE(image[0]); j++)
+		if(SDL_LockSurface(screenSurface) < 0)
 		{
-			image[i][j][RED] =
-				(i*(j/32))*255;
-				//(i%32) & (j%32);
+			return 1;
+		}
+	}
 
-			image[i][j][GREEN] =
-				0;
-			image[i][j][BLUE] =
-				0;
+	//get the time
+	int tick = SDL_GetTicks();
 
-			for(k = 0; k < 3; k++)
+	// Declare a couple of
+	int i = 0, j = 0, yofs = 0, ofs = 0;
+
+	rgbcolor pixel;
+
+	// Draw to
+	yofs = 0;
+	for(i = 0; i < SCREEN_HEIGHT; i++)
+	{
+		for(j = 0, ofs = yofs; j < SCREEN_WIDTH; j++, ofs++)
+		{
+			pixel[BLUE] =
+				(double)(i-HALF_SCREEN_HEIGHT)/8*(double)(j-HALF_SCREEN_WIDTH)/8+(double)tick/32;
+
+			pixel[GREEN] =
+				pixel[BLUE]*cos((double)i/32+(double)j/32);
+
+			pixel[RED] =
+				pixel[BLUE]*sin((double)i/32+(double)j/32);
+
+			((unsigned int*)screenSurface->pixels)[ofs] =
+				colortoint(pixel);
+		}
+		yofs += screenSurface->pitch / 4;
+	}
+
+	// Unlock if
+	if(SDL_MUSTLOCK(screenSurface))
+	{
+		SDL_UnlockSurface(screenSurface);
+	}
+
+	return 0;
+}
+
+int WinMain( int argc, char* args[] )
+{
+	printf("hello!\n");
+	char filename[256] = "output/UNINITIALIZED.ppm";
+	//The window we'll be rendering to
+	SDL_Window* window = NULL;
+
+	//The surface contained by the window
+	SDL_Surface* screenSurface = NULL;
+
+	//Initialize SDL
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	{
+		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+	}
+	else
+	{
+		//Create window
+		window = SDL_CreateWindow( "I C the Light", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+		if( window == NULL )
+		{
+			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+		}
+		else
+		{
+			//Get window surface
+			screenSurface = SDL_GetWindowSurface( window );
+
+			//save
+			render(screenSurface);
+			sprintf(filename, "output/image%lu.ppm", (unsigned long int)time(NULL));
+			if(writeppm(filename, PORTABLE_PIXMAP, SCREEN_WIDTH, SCREEN_HEIGHT, screenSurface->pixels)
+				!= 0)
+				printf("image write error!\n");
+		}
+	}
+	int quit = 0;
+	while (!quit)
+	{
+		//SDL_Delay(16);
+		// render
+		render(screenSurface);
+
+		//Update the surface
+		SDL_UpdateWindowSurface( window );
+
+		// poll forevents, and handle the ones we care about.
+		SDL_Event event;
+		while (SDL_PollEvent(&event))
+		{
+			switch (event.type)
 			{
-				min = image[i][j][k] < min ? image[i][j][k] : min;
-				max = image[i][j][k] > min ? image[i][j][k] : max;
+				case SDL_KEYUP:
+					// if escape is pressed, quit
+					if(event.key.keysym.sym == SDLK_ESCAPE)
+					{
+						quit = 1;
+					}
+					else if(event.key.keysym.sym == SDLK_s)
+					{
+						sprintf(filename, "output/image%lu.ppm", (unsigned long int)time(NULL));
+						if(writeppm(filename, PORTABLE_PIXMAP, SCREEN_WIDTH, SCREEN_HEIGHT, screenSurface->pixels)
+							!= 0)
+							printf("image write error!\n");
+					}
+					break;
+
+				case SDL_QUIT:
+					quit = 1;
+					break;
 			}
 		}
 	}
 
-	printf("min = %d, max = %d\n", min, max);
+	//Destroy window
+	SDL_DestroyWindow( window );
 
-	autoscale(256, 256, 3, image, min, max);
+	//Quit SDL subsystems
+	SDL_Quit();
 
-	char filename[256];
-	sprintf(filename, "output/image%lu.ppm", (unsigned long int)time(NULL));
-	printf("image initialized!\nwriting to %s\n", filename);
-	writeppm(filename, PORTABLE_PIXMAP, 256, 256, 3, image);
-	printf("image written!\n");
 	return 0;
 }
--- readme.md
+++ readme.md
@@ -1,10 +1,69 @@
 # I C the Light
 
-*A lightweight and fast ray tracer written in C*
+*a distance estimating ray marcher written in C*
 
-This is in a non-working stage at the moment, so don't expect anything to do anything.
+On ray marching (from [Syntopia](http://blog.hvidtfeldts.net/index.php/2011/06/distance-estimated-3d-fractals-part-i/)):
 
-As soon as I have *something* working, I'll branch off to do further development.
+> Classic raytracing shoots one (or more) rays per pixel and calculates where the
+> rays intersect the geometry in the scene. Normally the geometry is described by
+> a set of primitives, like triangles or spheres, and some kind of spatial
+> acceleration structure is used to quickly identify which primitives intersect
+> the rays.
+>
+> Distance Estimation, on the other hand, is a ray marching technique.
+>
+> Instead of calculating the exact intersection between the camera ray and the
+> geometry, you proceed in small steps along the ray and check how close you are
+> to the object you are rendering. When you are closer than a certain threshold,
+> you stop. In order to do this, you must have a function that tells you how
+> close you are to the object: a Distance Estimator. The value of the distance
+> estimator tells you how large a step you are allowed to march along the ray,
+> since you are guaranteed not to hit anything within this radius.
+
+## Directory
+
+### Code
+
+`color.h`:
+Micro-library for converting colors between `(unsigned int) 0xFFFFFF` and `unsigned char *{0xFF, 0xFF, 0xFF}` formats.
+
+`test.c`:
+Unit tests. Currently only tests `color.h`.
+
+`icthelight.c`:
+The beef of the code lives here.
+Can be redirected to a log file for some supplementary (but non-essential) output.
+
+### Misc
+
+`license.txt`:
+A copy of the MIT license; see below.
+
+`Makefile`:
+Super simple makefile, crafted by hand.
+
+`readme.md`:
+This file.
+
+## Branches
+
+Approximately ordered from most to least stable.
+
+`master`:
+(Usually) stable builds and major progress.
+
+(Maybe some day) `beta`:
+Unstable builds but not necessarily a horrible mess.
+Will generally be far ahead from `master`.
+
+`mess`:
+Commits almost certainly guaranteed not to work.
+Do not check these out; they’re just for me to store work for later.
+
+## Coming soon...
+
+* Binary downloads of the SDL DLLs from my website
+* Gallery of output on my website
 
 ## License
 
